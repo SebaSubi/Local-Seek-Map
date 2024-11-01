6a28f0c1214e4bfa690ee01a0f79424da7f00d00
LukySand <luky.sand@gmail.com>
2024-11-01 10:02:07 -0300

    changed project layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,5 @@
 // import { Slot } from "expo-router"; //This will render the index.tsx file in the same folder
 import { Stack } from "expo-router"; //This will render the index.tsx file in the same folder
-import { View } from "react-native";
 import Header from "../components/Header";
 // This file is the layout fo rth ehole ap, if you want to have something always showing, you put it here, you can have
 // as many layouts as you want, and you can switch between them using the router
@@ -8,13 +7,14 @@ import Header from "../components/Header";
 
 export default function HomeLayout() {
   return (
-    <View className="flex-1 bg-white">
-      <Stack
-        screenOptions={{
+    <Stack>
+      <Stack.Screen
+        name="(tabs)"
+        options={{
           header: () => <Header title="Main" />,
           headerStyle: { backgroundColor: "#fff" },
         }}
       />
-    </View>
+    </Stack>
   );
 }
