c48a8c2ff989a2d8978614490ed6154802a6421c
LukySand <luky.sand@gmail.com>
2025-02-12 02:55:28 +0100

    fixed newUpdateLocal and Create local dimentions

--- app/CRUD/LocalCRUD/NewUpdateLocal.tsx
+++ app/CRUD/LocalCRUD/NewUpdateLocal.tsx
@@ -11,10 +11,14 @@ import { colors } from "../../../constants/colors";
 import { useLocalIdStore } from "../../../libs/localZustang";
 
 export default function UpdateLocal() {
-  const { id, name, location, wpp, instagram, facebook, webpage, image } =
-    useLocalSearchParams();
+  // const { id, name, location, wpp, instagram, facebook, webpage, image } =
+  //   useLocalSearchParams();
 
   const nameRef = useRef<{
+    getValue: () => string;
+    setValue: (value: string) => void;
+  } | null>(null);
+  const addressRef = useRef<{
     getValue: () => string;
     setValue: (value: string) => void;
   } | null>(null);
@@ -44,6 +48,7 @@ export default function UpdateLocal() {
   } | null>(null);
   //errorHandlers
   const [nameError, setNameError] = useState("");
+  const [addressError, setAddressError] = useState(""); //TODO: add the address error
   const [locationError, setLocationError] = useState("");
   const [whatsappError, setWhatsappError] = useState("");
   const [instagramError, setInstagramError] = useState("");
@@ -85,14 +90,15 @@ export default function UpdateLocal() {
 
   const handlePress = async () => {
     const name = nameRef.current?.getValue();
-    const location = locationRef?.current?.getValue();
-    const whatsapp = wppNumberRef?.current?.getValue();
-    const instagram = instagramRef?.current?.getValue();
-    const facebook = facebookRef?.current?.getValue();
-    const webpage = paginaWebRef?.current?.getValue();
-    const image = imgURLRef?.current?.getValue();
+    const location = locationRef.current?.getValue();
+    const address = addressRef.current?.getValue();
+    const whatsapp = wppNumberRef.current?.getValue();
+    const instagram = instagramRef.current?.getValue();
+    const facebook = facebookRef.current?.getValue();
+    const webpage = paginaWebRef.current?.getValue();
+    const image = imgURLRef.current?.getValue();
 
-    if (!name || !location || !image) {
+    if (!name || !location || !image || !location || !address) {
       Alert.alert("Error", "Por favor complete todos los campos obligatorios.");
       return;
     }
@@ -103,6 +109,7 @@ export default function UpdateLocal() {
       setInstagramError("");
       setFacebookError("");
       setWebpageError("");
+      setAddressError("");
       return;
     }
     if (name.length >= 40) {
@@ -112,6 +119,7 @@ export default function UpdateLocal() {
       setInstagramError("");
       setFacebookError("");
       setWebpageError("");
+      setAddressError("");
       return;
     }
     if ((await checkLocalName(name)) === "true") {
@@ -121,10 +129,11 @@ export default function UpdateLocal() {
       setInstagramError("");
       setFacebookError("");
       setWebpageError("");
+      setAddressError("");
       return;
     }
 
-    if (location.length < 5) {
+    if (address.length < 5) {
       setNameError("");
       setLocationError(
         "La ubicacion del Local requiere minimamente 5 caracteres"
@@ -133,26 +142,53 @@ export default function UpdateLocal() {
       setInstagramError("");
       setFacebookError("");
       setWebpageError("");
+      setAddressError("");
       return;
     }
-    if (location.length >= 50) {
+    if (address.length >= 120) {
       setNameError("");
       setLocationError("La ubicacion del Local tiene demasiados caracteres");
       setWhatsappError("");
       setInstagramError("");
       setFacebookError("");
       setWebpageError("");
+      setAddressError("");
+      return;
+    }
+    if (location.length < 5) {
+      setNameError("");
+      setLocationError(
+        // eslint-disable-next-line prettier/prettier
+        "Las coordenadas del Local requieren minimamente 23 caracteres"
+      );
+      setWhatsappError("");
+      setInstagramError("");
+      setFacebookError("");
+      setWebpageError("");
+      setAddressError("");
+      return;
+    }
+    if (location.length >= 120) {
+      setNameError("");
+      setLocationError("Las coordenadas del Local tiene demasiados caracteres");
+      setWhatsappError("");
+      setInstagramError("");
+      setFacebookError("");
+      setWebpageError("");
+      setAddressError("");
       return;
     }
     if (whatsapp && whatsapp.length < 8) {
       setNameError("");
       setLocationError("");
       setWhatsappError(
+        // eslint-disable-next-line prettier/prettier
         "La longitud minima de un numero de Whatsapp es de 8 numeros"
       );
       setInstagramError("");
       setFacebookError("");
       setWebpageError("");
+      setAddressError("");
       return;
     }
     if (whatsapp && whatsapp.length > 18) {
@@ -160,11 +196,13 @@ export default function UpdateLocal() {
       setNameError("");
       setLocationError("");
       setWhatsappError(
+        // eslint-disable-next-line prettier/prettier
         "La longitud maxima de un numero de Whatsapp es de 18 numeros"
       );
       setInstagramError("");
       setFacebookError("");
       setWebpageError("");
+      setAddressError("");
       return;
     }
     if (instagram && instagram.length < 1) {
@@ -172,10 +210,12 @@ export default function UpdateLocal() {
       setLocationError("");
       setWhatsappError("");
       setInstagramError(
+        // eslint-disable-next-line prettier/prettier
         "La longitud minima de un usuario de instagram es de 1 caracter"
       );
       setFacebookError("");
       setWebpageError("");
+      setAddressError("");
       return;
     }
     if (instagram && instagram.length >= 30) {
@@ -183,10 +223,12 @@ export default function UpdateLocal() {
       setLocationError("");
       setWhatsappError("");
       setInstagramError(
+        // eslint-disable-next-line prettier/prettier
         "La longitud maxima de un usuario de instagram es de 30 caracteres"
       );
       setFacebookError("");
       setWebpageError("");
+      setAddressError("");
       return;
     }
     if (instagram && instagram.includes(",")) {
@@ -196,6 +238,7 @@ export default function UpdateLocal() {
       setInstagramError("un usuario de instagram no puede tener comas ','");
       setFacebookError("");
       setWebpageError("");
+      setAddressError("");
       return;
     }
     if (instagram && instagram.includes(" ")) {
@@ -205,6 +248,7 @@ export default function UpdateLocal() {
       setInstagramError("un usuario de instagram no puede tener espacios");
       setFacebookError("");
       setWebpageError("");
+      setAddressError("");
       return;
     }
     if (instagram && (instagram.includes("__") || instagram.includes(".."))) {
@@ -214,6 +258,7 @@ export default function UpdateLocal() {
       setInstagramError("un usuario de instagram no puede tener este caracter");
       setFacebookError("");
       setWebpageError("");
+      setAddressError("");
       return;
     }
     if (facebook && facebook.length < 5) {
@@ -222,9 +267,11 @@ export default function UpdateLocal() {
       setWhatsappError("");
       setInstagramError("");
       setFacebookError(
+        // eslint-disable-next-line prettier/prettier
         "un usuario de Facebook debe tener como minimo 5 caracteres"
       );
       setWebpageError("");
+      setAddressError("");
       return;
     }
     if (facebook && facebook.length > 50) {
@@ -233,9 +280,11 @@ export default function UpdateLocal() {
       setWhatsappError("");
       setInstagramError("");
       setFacebookError(
+        // eslint-disable-next-line prettier/prettier
         "un usuario de Facebook debe tener como maximo 50 caracteres"
       );
       setWebpageError("");
+      setAddressError("");
       return;
     }
     if (
@@ -251,9 +300,10 @@ export default function UpdateLocal() {
       setInstagramError("");
       setFacebookError("Un usuario de Facebook no permite estos caracteres");
       setWebpageError("");
+      setAddressError("");
       return;
     }
-    if (webpage && !verifyUrl(webpage)) {
+    if ((webpage && !verifyUrl(webpage)) || !verifyUrl(`https://${webpage}`)) {
       setNameError("");
       setLocationError("");
       setWhatsappError("");
@@ -275,7 +325,9 @@ export default function UpdateLocal() {
       dateFrom: new Date(),
     };
     console.log(newLocal);
-    await updateLocal(id as string, newLocal);
+    if (local.id) {
+      await updateLocal(local.id, newLocal);
+    }
   };
 
   return (
@@ -288,7 +340,7 @@ export default function UpdateLocal() {
       <View className="flex flex-row justify-between w-full items-center mb-2">
         <GoBackButton style="bg-white w-12 h-8 justify-center ml-3" />
         <Text className="text-white font-semibold text-xl mt-1 w-3/4 text-center">
-          {`Actualizar Local ${name === undefined ? "" : (name as string)}`}
+          {`Actualizar Local ${local.name === undefined ? "" : (local.name as string)}`}
         </Text>
         <Text style={{ color: colors.primary.blue }}>aaaaaa</Text>
       </View>
@@ -305,15 +357,27 @@ export default function UpdateLocal() {
           textStyle="mt-4"
           ref={nameRef}
         />
+        {addressError === "" ? null : (
+          <View className="w-full flex items-start ml-28">
+            <Text className="text-red-800">{addressError}</Text>
+          </View>
+        )}
+        <BasicTextInput
+          placeholder="Ubicacion de Local"
+          inputType="text"
+          title="Nueva Ubicación del Local"
+          textStyle="mt-4"
+          ref={addressRef}
+        />
         {locationError === "" ? null : (
           <View className="w-full flex items-start ml-28">
             <Text className="text-red-800">{locationError}</Text>
           </View>
         )}
         <BasicTextInput
-          placeholder="Ubicacion de Local"
+          placeholder="Coordenadas de Local"
           inputType="text"
-          title="Nueva Ubicación del Local" //This we will have to change later, since the person most likely wont knoe the coordinates
+          title="Coordenadas del Local"
           textStyle="mt-4"
           ref={locationRef}
         />
